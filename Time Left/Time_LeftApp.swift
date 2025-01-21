f63aabbf97cf219351a51dc49bf685a76abc14a1
Tyler <tyler.visca.barca@gmail.com>
2025-01-21 11:35:12 -0800

    Initial Commit

---
+++ Time Left/Time_LeftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_LeftApp.swift
+//  Time Left
+//
+//  Created by Tyler Rathod on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Time_LeftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Time LeftTests/Time_LeftTests.swift
@@ -0,0 +1,17 @@
+//
+//  Time_LeftTests.swift
+//  Time LeftTests
+//
+//  Created by Tyler Rathod on 1/21/25.
+//
+
+import Testing
+@testable import Time_Left
+
+struct Time_LeftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
