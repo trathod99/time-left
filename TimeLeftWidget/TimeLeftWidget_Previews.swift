e0a70e14e2dae939e57db818f4fccb1288deaf78
Tyler <tyler.visca.barca@gmail.com>
2025-01-22 16:23:30 -0800

    Working weeks of your life large widget

--- TimeLeftWidget/TimeLeftWidget_Previews.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-import WidgetKit
-
-struct TimeLeftWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        TimeLeftWidgetEntryView(entry: SimpleEntry(date: Date(), weeksThisYear: 26))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
-} 
